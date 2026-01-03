6b0f1cb620f9497f4dff2e24130e34f00e9e26eb
Seriki yakub <sandraisom00@gmail.com>
2026-01-03 04:27:28 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -29,3 +29,22 @@ sudo make install
           |  C++ Daemon |
           +-------------+
        (Matrix chat control)
+
+# Clone your qmatrixclient repo (or use your source folder)
+git clone <https://github.com/quotient-im/libQuotient.git> qmatrixclient
+cd libQuotient
+
+git clone <https://github.com/matrix-org/matrix.to.git> qmatrixclient
+cd matrix.to
+
+# Create a build folder
+mkdir build && cd build
+
+# Configure with cmake
+cmake ..
+
+# Compile
+make -j$(nproc)
+
+# Optional: Install system-wide
+sudo make install
