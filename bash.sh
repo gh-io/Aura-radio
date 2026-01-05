e105697b60fabb61b94bf89816412ccf5c2f50b9
Seriki yakub <sandraisom00@gmail.com>
2026-01-05 19:00:32 +0100

    Update and rename bash to bash.sh

--- bash.sh
+++ bash.sh
@@ -4,3 +4,8 @@ sudo apt install icecast2
 DJ / Mixxx / CLI -> Icecast (/live.ogg) -> Browser Audio
                                  -> WebSocket -> Now Playing + Listeners
 Backend REST API -> Playlist / Related Tracks -> UI Queue
+
+git clone --recursive https://gitlab.xiph.org/xiph/icecast-server.git
+git clone https://gitlab.xiph.org/xiph/icecast-server.git
+cd icecast-server
+git submodule update --init
