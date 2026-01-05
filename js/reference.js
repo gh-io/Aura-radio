d2600d23b73cc7678daf1140b1f3d87a512e07f3
Seriki yakub <sandraisom00@gmail.com>
2026-01-05 15:01:14 +0100

    Create reference.js

---
+++ js/reference.js
@@ -0,0 +1,19 @@
+const DOM = {
+    audio: document.getElementById("player-audio"),
+
+    serverUrl: document.getElementById("server-url"),
+    filters: document.getElementById("filters"),
+    sort: document.getElementById("sort-string"),
+
+    resultsList: document.getElementById("results-list"),
+    queueList: document.getElementById("queue-list"),
+
+    makeRequest: document.getElementById("make-request"),
+    nextTrack: document.getElementById("next-track"),
+
+    clearSelection: document.getElementById("clear-selection"),
+    prependQueue: document.getElementById("prepend-queue"),
+    appendQueue: document.getElementById("append-queue"),
+    removeQueue: document.getElementById("remove-queue"),
+    playSelected: document.getElementById("play-selected"),
+};
